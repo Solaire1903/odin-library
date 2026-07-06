d72d083bc942f47cf3b7e699173029f781c9a333
Solaire1903 <marvkie@gmail.com>
2026-07-06 17:31:47 +0200

    Add function to display the books in the library

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-function Book(title, author, pages, releaseDate) {
-    this.title = title;
+function Book(author, title, pages, releaseDate) {
     this.author = author;
+    this.title = title;
     this.pages = pages;
     this.releaseDate = releaseDate;
     this.id = crypto.randomUUID();
 }
 
-function addBook(title, author, pages, releaseDate) {
-    library.push(new Book(title, author, pages, releaseDate));
+function addBookToLibrary(author, title, pages, releaseDate) {
+    library.push(new Book(author, title, pages, releaseDate));
+}
+
+function displayLibrary() {
+    const bookDisplay = document.querySelector(".book-display");
+
+    for (const book of library) {
+        const bookElement = document.createElement("div");
+        bookElement.classList.add("book");
+
+        const author = document.createElement("p");
+        author.classList.add("author");
+        author.textContent = book.author;
+        bookElement.appendChild(author);
+
+        const title = document.createElement("p");
+        title.classList.add("title");
+        title.textContent = book.title;
+        bookElement.appendChild(title);
+
+        const pages = document.createElement("p");
+        pages.classList.add("pages");
+        pages.textContent = `${book.pages} pages`;
+        bookElement.appendChild(pages);
+
+        const releaseDate = document.createElement("p");
+        releaseDate.classList.add("release-date");
+        releaseDate.textContent = `Released in: ${book.releaseDate}`;
+        bookElement.appendChild(releaseDate);
+
+        bookDisplay.appendChild(bookElement);
+    }
 }
 
 const library = [];
+
+displayLibrary();
