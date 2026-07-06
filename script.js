8816bab492583eb5f6235dd623ff3eef6f271f7d
Solaire1903 <marvkie@gmail.com>
2026-07-06 15:45:59 +0200

    Add array for storing books and a function to add a book to the array

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ function Book(title, author, pages, releaseDate) {
     this.id = crypto.randomUUID();
 }
 
+function addBook(title, author, pages, releaseDate) {
+    library.push(new Book(title, author, pages, releaseDate));
+}
 
+const library = [];
